d277a4264e34f3ced2d632f7815cca573c43510c
Furkan Demir <Furkan Demir@FURKAN>
2023-04-09 00:10:30 +0300

    VM'ler oluşturuldu Layout düzenlendi,AdminScript yazıldı ve CategoryController BackEnd ve FrontEnd tamamen düzenlendi ve test Edildi

--- Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
+++ Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Project.BLL.Repositories.ConcRep;
+using Project.ENTITIES.Models;
 using Project.MVCUI.Areas.Admin.Data.AdminPageVMs;
 using Project.VM.PureVMs;
 using System;
@@ -16,22 +17,22 @@ namespace Project.MVCUI.Areas.Admin.Controllers
         {
             _cRep = new CategoryRepository();
         }
+    
         public ActionResult ListCategories(int? id)
         {
             List<AdminCategoryVM> categories;
-            if (id == null)
+            if(id == null)
             {
                 categories = _cRep.Select(x => new AdminCategoryVM
                 {
-                    ID = x.ID,
-                    CategoryName = x.CategoryName,
-                    Description = x.Description,
-                    DeletedDate = x.DeletedDate,
-                    ModifiedDate = x.ModifiedDate,
-                    CreatedDate = x.CreatedDate,
-                    Status = x.Status.ToString(),
-                }).ToList();
-                
+					ID = x.ID,
+					CategoryName = x.CategoryName,
+					DeletedDate = x.DeletedDate,
+					CreatedDate = x.CreatedDate,
+					Description = x.Description,
+					ModifiedDate = x.ModifiedDate,
+					Status = x.Status.ToString(),
+				}).ToList();    
             }
             else
             {
@@ -43,7 +44,7 @@ namespace Project.MVCUI.Areas.Admin.Controllers
                     CreatedDate = x.CreatedDate,
                     Description = x.Description,
                     ModifiedDate = x.ModifiedDate,
-                    Status = x.Status.ToString()
+                    Status = x.Status.ToString(),
                 }).ToList();
             }
             AdminCategoryListPageVM aclpvm = new AdminCategoryListPageVM
@@ -52,5 +53,57 @@ namespace Project.MVCUI.Areas.Admin.Controllers
             };
             return View(aclpvm);
         }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(AdminCategoryVM item)
+        {
+            Category c = new Category
+            {
+                CategoryName = item.CategoryName,
+                Description = item.Description,
+                ID = item.ID,
+            };
+            _cRep.Add(c);
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult UpdateCategory(int id )
+        {
+            Category selected = _cRep.Find(id);
+
+            AdminAddUpdateCategoryPageVM acpvm = new AdminAddUpdateCategoryPageVM
+            {
+                Category = new AdminCategoryVM
+                {
+                    ID = selected.ID,
+                    CategoryName = selected.CategoryName,
+                    Description = selected.Description
+                }
+            };
+            return View(acpvm);
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateCategory(AdminCategoryVM category)
+        {
+            Category toBeUpdated = _cRep.Find(category.ID);
+
+            toBeUpdated.CategoryName = category.CategoryName;
+            toBeUpdated.Description = category.Description;
+            _cRep.Update(toBeUpdated);
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("ListCategories");
+        }
     }
+
 }
---
+++ Project.MVCUI/Areas/Admin/Data/AdminPageVMs/AdminAddUpdateCategoryPageVM.cs
@@ -0,0 +1,13 @@
+﻿using Project.VM.PureVMs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.MVCUI.Areas.Admin.Data.AdminPageVMs
+{
+	public class AdminAddUpdateCategoryPageVM
+	{
+		public AdminCategoryVM Category { get; set; }
+	}
+}
